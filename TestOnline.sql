60461f5d7fe02d294faa5d2f63d38e7de71b4b2d
StephenLER <2903838895@qq.com>
2024-05-19 22:00:19 +0800

    习思想问卷

---
+++ TestOnline.sql
@@ -0,0 +1,7 @@
+SELECT `question`.`question_id`,
+    `question`.`question_title`,
+    `question`.`question_difficulty`,
+    `question`.`subject_id`,
+    `question`.`type_id`,
+    `question`.`knowledge_id`
+FROM `exam_online_system`.`question`;
